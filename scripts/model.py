67d34860a859d432ff5c96fd4fa57ce5930ba37b
ahuertas <ahuertas@resolvestudio.co>
2018-02-21 23:27:07 +0000

    first commit2

--- scripts/model.py
+++ scripts/model.py
@@ -3,22 +3,38 @@
 # file to build the keras linknet model
 
 from keras.models import Sequential
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+#from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.layers import Conv2D , MaxPooling2D 
 from keras.layers.core import Activation, Dense, Flatten, Dropout
 from keras.optimizers import Adam
 from keras.regularizers import l2
 from keras import backend as K
 
 
-def dice_error( img , mask ):
+input_shape = ( )
+
+def loss( img , mask ):
+
+
+	return 1  - bce(img , mask) + dice(img , mask )
+def dice( img , mask ):
 
 	# do something
 
-def bce( img , mask , ws) :
+def bce( img , mask ) :
 	# cross entropy con pesos
 
 def build_model():
 
 	#Build the LinkNetModel
-	return None
+
+	model = Sequential()
+
+	model.add( )
+
+	model.add( Conv2D(  64 , kernel_size=(2,2),  input_shape = input_shape  )  )
+	
+	model.compile(optimizer=adam, loss=root_mean_squared_error)
+
+	return model 
 
