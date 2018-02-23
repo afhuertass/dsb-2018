4e242afe3ca314d3b1afa8c934274b4a3e425a97
afhuertass <afhuertass@unal.edu.co>
2018-02-22 22:24:37 -0500

    mask experiments

--- scripts/model.py
+++ scripts/model.py
@@ -22,11 +22,11 @@ premodels = {
 }
 
 
-def jaccard_loss( y_true , y_pred): 
+def jaccard( y_true , y_pred): 
 	# implementation of jaccard distance loss 
 	# recordar que la mascara esta contamidada con los pesos 
 	smooth = 100 
-
+	
 	intersection = K.sum( K.abs( y_true*y_pred ) , axis = -1   )
 	sum_ = K.sum( K.abs( y_true )  + K.abs( y_pred ) , axis = -1 )
 	jac = (intersection + smooth)/( sum_ - intersection + smooth )
