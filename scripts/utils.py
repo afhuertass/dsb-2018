eabb7ef9efc25f7b806e490f735b58bd32fe7c36
afhuertass <afhuertass@unal.edu.co>
2018-03-25 17:48:29 -0500

    minor changes and small prediction routine

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,3 +2,20 @@ import numpy as np
 import pandas as pd 
 
 # utilidades varias 
+
+
+def rle_encoding(x):
+    '''
+    x: numpy array of shape (height, width), 1 - mask, 0 - background
+    Returns run length as list
+    '''
+    dots = np.where(x.T.flatten()==1)[0] # .T sets Fortran order down-then-right
+
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b+1, 0))
+        run_lengths[-1] += 1
+        prev = b
+        
+    return run_lengths
